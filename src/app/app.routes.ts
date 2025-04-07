e1ae144ee3abbeca780aaaff669677930a2a060e
molmos <molmos@circontrol.com>
2025-04-07 20:39:28 +0200

    Implement LinkedIn login functionality and update routing configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
-import { AuthGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
-  {
+  /*   {
     path: '',
-    canActivate: [AuthGuard]
-  }
+    canActivate: [AuthGuard],
+  } */
 ];
