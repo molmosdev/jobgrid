b560584d44be610941b6b4123d1fec32f86a8de7
molmos <molmos@circontrol.com>
2025-04-08 11:04:44 +0200

    Uncomment route configuration for the main application component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
-  /*   {
+  {
     path: '',
     canActivate: [AuthGuard],
-  } */
+    loadComponent: () => import('./app.component').then((m) => m.AppComponent)
+  }
 ];
