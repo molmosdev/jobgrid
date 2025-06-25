56b4a60f506c8d5f124e73f89126a2a87fbe0f93
molmosdev <hello@molmos.dev>
2025-06-25 19:32:31 +0200

    Add user authentication buttons to app component and include JsonPipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, computed, inject } from '@angular/core';
 import { AuthService } from './core/services/auth.service';
 import { User } from './shared/interfaces/user';
+import { JsonPipe } from '@angular/common';
 
 /**
  * Root component of the JobGrid application.
@@ -8,7 +9,8 @@ import { User } from './shared/interfaces/user';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [JsonPipe]
 })
 export class AppComponent {
   title = 'JobGrid';
