d694fb219acd2db74697ce4c2241adc3775f1258
molmosdev <hello@molmos.dev>
2025-08-05 21:55:24 +0200

    Implement OnInit lifecycle hook and inject ThemeService for theme management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, computed, inject, OnInit } from '@angular/core';
 import { AuthService } from './core/services/auth.service';
 import { User } from './shared/interfaces/user';
 import { RouterOutlet } from '@angular/router';
+import { ThemeService } from '@basis-ng/primitives';
 
 /**
  * Root component of the JobGrid application.
@@ -11,10 +12,15 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet],
   templateUrl: './app.component.html'
 })
-export default class AppComponent {
+export default class AppComponent implements OnInit {
   title = 'JobGrid';
   authService = inject(AuthService);
   readonly user = computed<User | null>(() => this.authService.user());
+  themeService = inject(ThemeService);
+
+  ngOnInit(): void {
+    this.themeService.applyTheme('light');
+  }
 
   login() {
     this.authService.logInWithLinkedIn();
