52720e6ecaaceafae03b7699963e2485c3e9abbe
molmosdev <hello@molmos.dev>
2025-08-05 21:55:57 +0200

    Implement registration form with validation and dynamic URL generation

--- src/app/features/register/register.component.ts
+++ src/app/features/register/register.component.ts
@@ -1,9 +1,36 @@
 import { Component } from '@angular/core';
+import {
+  ButtonComponent,
+  CardComponent,
+  InputComponent,
+  CardContentComponent,
+  CardFooterComponent,
+  InputGroupComponent
+} from '@basis-ng/primitives';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [
+    InputComponent,
+    ButtonComponent,
+    CardComponent,
+    CardContentComponent,
+    CardFooterComponent,
+    InputGroupComponent,
+    ReactiveFormsModule
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export default class RegisterComponent {}
+export default class RegisterComponent {
+  registerForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    domain: new FormControl('', Validators.required),
+    subdomain: new FormControl('', Validators.required)
+  });
+
+  onSubmit() {
+    this.registerForm.markAllAsTouched();
+  }
+}
