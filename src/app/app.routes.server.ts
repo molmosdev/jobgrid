84da6ff0f3a85e8d8a6a6f0a85602f5a07b7297e
molmosdev <hello@molmos.dev>
2025-08-30 20:44:06 +0200

    feat: add server routes for authentication and dashboard navigation

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,5 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   { path: '', renderMode: RenderMode.Server },
+  { path: 'coming-soon', renderMode: RenderMode.Server },
+  { path: 'home', renderMode: RenderMode.Server },
+  { path: 'auth', renderMode: RenderMode.Server },
+  { path: 'auth/login', renderMode: RenderMode.Server },
+  { path: 'auth/register', renderMode: RenderMode.Server },
+  { path: 'dashboard', renderMode: RenderMode.Server },
+  { path: 'dashboard/create-company', renderMode: RenderMode.Server },
+  { path: 'dashboard/overview', renderMode: RenderMode.Server },
+  { path: 'dashboard/settings', renderMode: RenderMode.Server },
   { path: '**', renderMode: RenderMode.Client },
 ];
